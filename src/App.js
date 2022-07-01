90748485c6cd52f5b1f089d5edd56a76ed6527ce
Bollgade <Bollgade@lol.com>
2022-07-01 18:40:25 +0300

    Создание модального окна и внесение в него формы

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import PostFilter from "./components/PostFilter";
 import PostForm from "./components/PostForm";
 // import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyModal from "./components/UI/MyModal/MyModal";
 
 // import ClassCounter from "./components/ClassCounter";
 // import Counter from "./components/Counter";
@@ -15,8 +17,8 @@ function App() {
     { id: 2, title: 'JavaScript 2', body: 'Description' },
     { id: 3, title: 'JavaScript 3', body: 'Description' },
   ]);
-
   const [filter, setFilter] = useState({ sort: '', query: '' });
+  const [modal, setModal] = useState(false);
 
   const sortedPosts = useMemo(() => {
     if (filter.sort) {
@@ -31,6 +33,7 @@ function App() {
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
+    setModal(false);
   };
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id));
@@ -38,7 +41,12 @@ function App() {
 
   return (
     <div className="app">
-      <PostForm create={createPost} />
+      <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
+        Создать пост
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
       <hr style={{ margin: '15px 0' }} />
       <PostFilter
         filter={filter}
