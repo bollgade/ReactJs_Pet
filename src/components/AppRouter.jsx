9e29b4a8632302b8a2fb258d5ad720347e768245
Bollgade <Bollgade@lol.com>
2022-07-24 20:20:31 +0300

    Разграничение доступа для неавторизованного юзера

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -5,16 +5,26 @@ import Posts from '../pages/Posts';
 import { Navigate } from 'react-router-dom';
 import Error from './../pages/Error';
 import PostIdPage from './../pages/PostIdPage';
-import { routes } from './../router/routes';
+import { publicRoutes, privateRoutes } from './../router/routes';
 
 const AppRouter = () => {
+  const isAuth = false;
   return (
-    <Routes>
-      {routes.map(route =>
-        <Route path={route.path} element={< route.element />} exact={route.exact} />
-      )}
-      <Route path="/*" element={<Navigate to="/posts" />} />
-    </Routes>
+    isAuth
+      ?
+      <Routes>
+        {privateRoutes.map(route =>
+          <Route path={route.path} element={< route.element />} exact={route.exact} />
+        )}
+        <Route path="/*" element={<Navigate to="/posts" />} />
+      </Routes>
+      :
+      <Routes>
+        {publicRoutes.map(route =>
+          <Route path={route.path} element={< route.element />} exact={route.exact} />
+        )}
+        <Route path="/*" element={<Navigate to="/login" />} />
+      </Routes>
   );
 };
 
