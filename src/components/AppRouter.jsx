0654a7da469d4ca83343813db50482c280e73171
Bollgade <Bollgade@lol.com>
2022-07-24 17:50:09 +0300

    Декомпозиция routes

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -5,15 +5,15 @@ import Posts from '../pages/Posts';
 import { Navigate } from 'react-router-dom';
 import Error from './../pages/Error';
 import PostIdPage from './../pages/PostIdPage';
+import { routes } from './../router/routes';
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/about" element={<About />} />
-      <Route path="/posts" element={<Posts />} />
-      <Route path="/posts/:id" element={<PostIdPage />} />
-      <Route path="/error" element={<Error />} />
-      <Route path="/*" element={<Navigate to="/error" />} />
+      {routes.map(route =>
+        <Route path={route.path} element={< route.element />} exact={route.exact} />
+      )}
+      <Route path="/*" element={<Navigate to="/posts" />} />
     </Routes>
   );
 };
