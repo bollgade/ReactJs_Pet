9312537574e6ab36c29f3cfbd5e1751bf0a773f9
Bollgade <Bollgade@lol.com>
2022-07-24 15:45:42 +0300

    Страница поста, загрузка комментов

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -4,12 +4,14 @@ import About from '../pages/About';
 import Posts from '../pages/Posts';
 import { Navigate } from 'react-router-dom';
 import Error from './../pages/Error';
+import PostIdPage from './../pages/PostIdPage';
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/about" element={<About />} />
       <Route path="/posts" element={<Posts />} />
+      <Route path="/posts/:id" element={<PostIdPage />} />
       <Route path="/error" element={<Error />} />
       <Route path="/*" element={<Navigate to="/error" />} />
     </Routes>
