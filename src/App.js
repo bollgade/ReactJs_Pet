0aba08032cecd76daa8f3b014481c47f1d2490a4
Bollgade <Bollgade@lol.com>
2022-07-28 20:07:19 +0300

    useContext; запись в хранилище авторизован ли

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import About from "./pages/About";
 import "./styles/App.css";
 import Posts from './pages/Posts';
 import Navbar from "./components/UI/Navbar/Navbar";
 import AppRouter from "./components/AppRouter.jsx";
+import { AuthContext } from "./context";
+import { useEffect } from "react";
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (localStorage.getItem('auth')) {
+      setIsAuth(true);
+    };
+    setLoading(false);
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Navbar />
-      <AppRouter />
-    </BrowserRouter>
+    <AuthContext.Provider value={{
+      isAuth,
+      setIsAuth,
+      isLoading
+    }}>
+      <BrowserRouter>
+        <Navbar />
+        <AppRouter />
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 
--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import About from '../pages/About';
 import Posts from '../pages/Posts';
@@ -6,22 +6,29 @@ import { Navigate } from 'react-router-dom';
 import Error from './../pages/Error';
 import PostIdPage from './../pages/PostIdPage';
 import { publicRoutes, privateRoutes } from './../router/routes';
+import { AuthContext } from './../context/index';
+import Loader from './UI/Loader/Loader';
 
 const AppRouter = () => {
-  const isAuth = false;
+  const { isAuth, isLoading } = useContext(AuthContext);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return (
     isAuth
       ?
       <Routes>
         {privateRoutes.map(route =>
-          <Route path={route.path} element={< route.element />} exact={route.exact} />
+          <Route path={route.path} element={< route.element />} exact={route.exact} key={route.path} />
         )}
         <Route path="/*" element={<Navigate to="/posts" />} />
       </Routes>
       :
       <Routes>
         {publicRoutes.map(route =>
-          <Route path={route.path} element={< route.element />} exact={route.exact} />
+          <Route path={route.path} element={< route.element />} exact={route.exact} key={route.path} />
         )}
         <Route path="/*" element={<Navigate to="/login" />} />
       </Routes>
