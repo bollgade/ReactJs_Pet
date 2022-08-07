5bf9945ca17133129fac2ef434ca29c8af4e48f9
Bollgade <Bollgade@lol.com>
2022-08-07 10:20:24 +0300

    dynamic line when scroll + change limit on page

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import PostFilter from "../components/PostFilter";
 import PostForm from "../components/PostForm";
 import PostList from "../components/PostList";
@@ -11,6 +11,8 @@ import Loader from '../components/UI/Loader/Loader';
 import { useFetching } from '../hooks/useFetching';
 import { getPageCount } from '../utils/pages';
 import Pagination from "../components/UI/pagination/Pagination";
+import { useObserver } from './../hooks/useObserver';
+import MySelect from './../components/UI/select/MySelect';
 
 function Posts() {
   const [posts, setPosts] = useState([]);
@@ -20,11 +22,11 @@ function Posts() {
   const [limit, setLimit] = useState(10);
   const [page, setPage] = useState(1);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-
+  const lastElement = useRef();
 
   const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
     const response = await PostService.getAll(limit, page);
-    setPosts(response.data);
+    setPosts([...posts, ...response.data]);
     const totalCount = response.headers['x-total-count'];
     setTotalPages(getPageCount(totalCount, limit));
   });
@@ -34,9 +36,13 @@ function Posts() {
     setModal(false);
   };
 
+  useObserver(lastElement, page < totalPages, isPostsLoading, () => {
+    setPage(page + 1);
+  });
+
   useEffect(() => {
     fetchPosts(limit, page);
-  }, []);
+  }, [page, limit]);
 
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id));
@@ -44,7 +50,6 @@ function Posts() {
 
   const changePage = (page) => {
     setPage(page);
-    fetchPosts(limit, page);
   };
 
   return (
@@ -60,12 +65,24 @@ function Posts() {
         filter={filter}
         setFilter={setFilter}
       />
+      <MySelect
+        value={limit}
+        onChange={value => setLimit(value)}
+        defaultValue="Кол-во элементов на странице"
+        options={[
+          { value: 5, name: '5' },
+          { value: 10, name: '10' },
+          { value: 25, name: '25' },
+          { value: -1, name: 'Показать все' },
+        ]}
+      />
       {postError &&
         <h1>Произошла ошибка ${postError}</h1>
       }
-      {isPostsLoading
-        ? <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}><Loader /></div>
-        : <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Посты про JS' />
+      <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Посты про JS' />
+      <div ref={lastElement} style={{ height: 20, background: 'red' }} />
+      {isPostsLoading &&
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}><Loader /></div>
       }
       <Pagination
         page={page}
